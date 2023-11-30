3bf5a5468810eecf13fc026be697dbbd57867f14
lakoc <xpolok03@stud.fit.vutbr.cz>
2023-11-30 10:39:27 +0100

    WSJ dataset builder regeneration by local dir

--- recipes/wsj/prepare_dataset_BUT.sh
+++ recipes/wsj/prepare_dataset_BUT.sh
@@ -4,8 +4,8 @@
 #$ -l ram_free=2G,mem_free=2G
 #$ -l matylda5=0.05,matylda4=0.05
 #$ -pe smp 16
-#$ -o /mnt/matylda5/ipoloka/projects/huggingface_asr/wsj_v2.o
-#$ -e /mnt/matylda5/ipoloka/projects/huggingface_asr/wsj_v2.e
+#$ -o /mnt/matylda5/ipoloka/projects/huggingface_asr/wsj.o
+#$ -e /mnt/matylda5/ipoloka/projects/huggingface_asr/wsj.e
 
 # Limit job runtime to 24 h -> 86400 s, send SIGXCPU and SIGKILL if limit is breached
 ulimit -t 86400
@@ -25,7 +25,7 @@ unset PYTHONHOME
 source /mnt/matylda5/ipoloka/miniconda3/bin/activate /mnt/matylda5/ipoloka/envs/hugginface_asr
 
 # Ensure work directory exists
-METADATA_DIR="/mnt/matylda4/kesiraju/tools/kaldi-asr/kaldi/egs/wsj/s5/data/"
+METADATA_DIR="/mnt/matylda5/ipoloka/projects/huggingface_asr/metadata_dirs/wsj"
 WORK_DIR="/mnt/matylda5/ipoloka/projects/huggingface_asr"
 
 cd $WORK_DIR || {
@@ -35,9 +35,10 @@ cd $WORK_DIR || {
 export HF_DATASETS_OFFLINE=1
 export HF_HUB_OFFLINE=1
 export HF_HOME="${WORK_DIR}/hf_cache"
+export PATH="/mnt/matylda5/ipoloka/utils:$PATH"
 
 python src/dataset_builders/preprocess_dataset.py \
   --dataset_builder src/dataset_builders/kaldi_dataset \
   --metadata_dir $METADATA_DIR \
   --num_proc 16 \
-  --splits train_si284 dev_dt_20 dev_dt_05 test_eval92
+  --splits train dev_dt_20 dev_dt_05 test
