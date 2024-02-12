cbe63e0464451faf7f07b6ad06c53bb6eec13ba3
Lakoc <ipoloka@fit.vutbr.cz>
2024-02-12 10:08:04 +0100

    decoding jobs

---
+++ recipes/tedlium/decode_with_normalized_model.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/bash
+#SBATCH --job-name TED
+#SBATCH --account OPEN-28-57
+#SBATCH --partition qgpu_exp
+#SBATCH --time 01:00:00
+#SBATCH --nodes=1
+#SBATCH --gpus=4
+#SBATCH --output=/mnt/proj1/open-28-58/lakoc/huggingface_asr/outputs/ebranchformer_tedlium_small_normalized_v2_decoding.out
+
+EXPERIMENT="ebranchformer_tedlium_small_normalized_v2_decoding"
+PROJECT="regularizations_english_corpus"
+WORK_DIR="/mnt/proj1/open-28-58/lakoc/huggingface_asr"
+RECIPE_DIR="${WORK_DIR}/recipes/tedlium"
+EXPERIMENT_PATH="${WORK_DIR}/experiments/${EXPERIMENT}"
+HF_HOME="/scratch/project/open-28-57/lakoc/huggingface_cache"
+
+
+export HF_HOME="/scratch/project/open-28-57/lakoc/huggingface_cache"
+export PYTHONPATH="${PYTHONPATH}:${WORK_DIR}/src"
+export OMP_NUM_THREADS=64
+export WANDB_PROJECT=$PROJECT
+export WANDB_RUN_ID="${EXPERIMENT}"
+
+conda deactivate
+source activate loco_asr
+
+EXPERIMENT_PATH="${WORK_DIR}/experiments/${EXPERIMENT}"
+
+cd $WORK_DIR
+
+args=(
+  # General training arguments
+  --output_dir=$EXPERIMENT_PATH
+  --per_device_eval_batch_size="4"
+  --dataloader_num_workers="24"
+  --do_evaluate
+
+  # Data related arguments
+  --max_duration_in_seconds="20.0"
+  --min_duration_in_seconds="0.2"
+  --length_column_name="input_len"
+  --remove_unused_columns="False"
+  --preprocessing_num_workers="128"
+  --datasets_creation_config="${RECIPE_DIR}/tedlium_normalized.json"
+  --writer_batch_size="200"
+  --test_splits tedlium3_test
+
+  # Preprocessing related arguments
+  --data_preprocessing_config="${RECIPE_DIR}/data_preprocessing.json"
+
+  # Model related arguments
+  --tokenizer_name="Lakoc/ted_uni500_normalized"
+  --feature_extractor_name="Lakoc/log_80mel_extractor_16k"
+  --from_pretrained="/mnt/proj1/open-28-58/lakoc/huggingface_asr/experiments/ebranchformer_tedlium_small_normalized_v2/checkpoint-110040"
+  --expect_2d_input
+
+  # Generation related arguments
+  --filter_empty_labels
+  --num_beams="40"
+  --max_length="512"
+  --predict_with_generate
+  --decoding_ctc_weight="0.3"
+)
+
+torchrun --standalone --nnodes=1 --nproc-per-node=$SLURM_GPUS_ON_NODE src/trainers/train_enc_dec_asr.py "${args[@]}"
